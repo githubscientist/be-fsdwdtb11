79021a64868cb73108057862de2a2d3da71bb032
githubscientist <sathish.datascientist@gmail.com>
2024-12-27 19:48:24 +0530

    middlewares and writing custom middlewares

--- readme.md
+++ readme.md
@@ -3,8 +3,8 @@
 ## Topics
 
 - [x] What is ExpressJS?
-- [ ] Launch app.listen()
-- [ ] What is middleware?
+- [x] Launch app.listen()
+- [x] What is middleware?
 - [ ] Understanding inbuilt middlewares
 - [ ] Custom middlewares
 
@@ -13,3 +13,9 @@
 - ExpressJS is a minimal and flexible Node.js web application framework that provides a robust set of features for web applications.
 - It is an open-source framework that is used to build web applications in Node.
 - It is used over the vanilla Node.js http module because it provides a lot of features that are required in web development like routing, middleware, etc. and it is very easy to use and manage.
+
+### Launch app.listen()
+
+- The `app.listen()` function is used to bind and listen the connections on the specified host and port.
+
+### What is middleware?
--- server.js
+++ server.js
@@ -4,13 +4,36 @@ const express = require('express');
 // create an express app
 const app = express();
 
+// custom middleware for logging requests
+const logger = (request, response, next) => {
+    console.log(`Request URL: ${request.url}`);
+    console.log(`Request Method: ${request.method}`);
+    console.log(`Request Headers: ${JSON.stringify(request.headers)}`);
+    console.log(`Request Query: ${JSON.stringify(request.query)}`);
+    console.log(`Request Params: ${JSON.stringify(request.params)}`);
+    console.log(`Request Body: ${JSON.stringify(request.body)}`);
+    console.log(`Request Cookies: ${JSON.stringify(request.cookies)}`);
+    console.log(`-----------------------------`);
+
+    next();
+}
+
+// use the logger middleware
+app.use(logger);
+
 app.get('/', (request, response) => {
-    response.json({ message: 'GET /' });
+    response.json({ message: 'GET' });
 });
 
-app.post('/', (request, response) => {
-    response.json({ message: 'POST /' });
-});
+
+// middleware for handling 404 errors
+const notFound = (request, response, next) => {
+    response.json({ message: 'Route not found' });
+
+    next();
+}
+
+app.use(notFound);
 
 // listen for requests and start the server
 app.listen(3000, () => {
