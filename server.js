0aa6c7fb223d97a64410d218e6ab9bf735270786
githubscientist <sathish.datascientist@gmail.com>
2024-12-30 20:41:00 +0530

    config.js --adding abstraction to env variables

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const app = require("./app");
 const mongoose = require('mongoose');
-require('dotenv').config();
+const { MONGODB_URI, PORT } = require("./utils/config");
 
 // connect to the database
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(MONGODB_URI)
     .then(() => {
         console.log('Connected to the database...');
 
         // listen for requests and start the server
-        app.listen(process.env.PORT, () => {
+        app.listen(PORT, () => {
             console.log(`Server is running @ http://127.0.0.1:3000`);
         });
     })
